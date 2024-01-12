889baff99a54352da0343b9d2ade21c9bcaf918d
MaksymilianLipowski <156448934+MaksymilianLipowski@users.noreply.github.com>
2024-01-12 17:09:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-input.onSound(DetectedSound.Loud, function () {
-    basic.showString("Hello!")
+input.onButtonPressed(Button.A, function () {
+    led.unplot(position, 4)
+    position += -1
+    led.plot(position, 4)
 })
+input.onButtonPressed(Button.B, function () {
+    led.unplot(position, 4)
+    position += 1
+    led.plot(position, 4)
+})
+let position = 0
+position = 2
