9c68302085c6bbf339e05011498961fae0a640ca
MaksymilianLipowski <156448934+MaksymilianLipowski@users.noreply.github.com>
2024-01-12 17:54:15 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let position = 0
+let x = 0
+let y = 0
+let delay = 0
+let colision = 0
+let score = 0
 input.onButtonPressed(Button.A, function () {
     if (position >= 1) {
         led.unplot(position, 4)
@@ -8,7 +14,33 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showString("Good luck!")
+    x = 0
+    y = 0
+    delay = 500
+    colision = 0
+    position = 2
+    score = 0
+    led.plot(position, 4)
+    basic.pause(2000)
+    while (colision != 1) {
+        x = randint(0, 4)
+        for (let y = 0; y <= 4; y++) {
+            if (led.point(x, y)) {
+                music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+                score += 1
+            } else {
+                led.plot(x, y)
+                basic.pause(delay)
+                led.unplot(x, y)
+                if (y == 4) {
+                    colision = 1
+                }
+            }
+        }
+    }
+    basic.showString("" + (score))
+    music.play(music.stringPlayable("A F E F D G E F ", 120), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
     if (position <= 3) {
@@ -19,30 +51,3 @@ input.onButtonPressed(Button.B, function () {
         music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
 })
-let position = 0
-let x = 0
-let y = 0
-let delay = 500
-let colision = 0
-position = 2
-let score = 0
-led.plot(position, 4)
-basic.pause(100)
-while (colision != 1) {
-    x = randint(0, 4)
-    for (let y = 0; y <= 4; y++) {
-        if (led.point(x, y)) {
-            music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-            score += 1
-        } else {
-            led.plot(x, y)
-            basic.pause(delay)
-            led.unplot(x, y)
-            if (y == 4) {
-                colision = 1
-            }
-        }
-    }
-}
-basic.showString("" + (score))
-music.play(music.stringPlayable("A F E F D G E F ", 120), music.PlaybackMode.UntilDone)
